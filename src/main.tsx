95bf3514c05717af9d24e8716317aa403a750490
thepablosantos <iphonepablosantos@gmail.com>
2025-03-26 21:36:19 +0000

    chore: add GitHub Pages configuration

--- src/main.tsx
+++ src/main.tsx
@@ -8,22 +8,18 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      retry: 1,
-      refetchOnWindowFocus: true,
-      staleTime: 0,
-      gcTime: 1000 * 60 * 5, // 5 minutos
-      refetchOnMount: true,
-      refetchOnReconnect: true
+      gcTime: 1000 * 60 * 60 * 24,
+      staleTime: 1000 * 60 * 5,
     },
   },
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter basename="/multiversx-block-explorer">
         <App />
-      </QueryClientProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
